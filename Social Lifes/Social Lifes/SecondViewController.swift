eb68c4d62b2075912885486f02ea3632dc1fb536
Kyle Watson <dev.kyle.watson@gmail.com>
2014-07-15 02:01:12 -0400

    Setup as swift project

---
+++ Social Lifes/Social Lifes/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Social Lifes
+//
+//  Created by Kyle Watson on 7/15/14.
+//  Copyright (c) 2014 Kyle Watson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
