6545914391d1d0d752cb6b9fd1285b47d412995f
Kyle Watson <dev.kyle.watson@gmail.com>
2014-07-15 07:35:35 -0400

    rename VC

--- Social Lifes/Social Lifes/AddAccountsViewController.swift
+++ Social Lifes/Social Lifes/AddAccountsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AddAccountsViewController: UIViewController {
                             
 	override func viewDidLoad() {
 		super.viewDidLoad()
