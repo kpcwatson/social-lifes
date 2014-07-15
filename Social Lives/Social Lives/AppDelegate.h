3b9616101f84ba7754eab889e93f3d205c162b99
Kyle Watson <dev.kyle.watson@gmail.com>
2014-07-15 01:13:29 -0400

    initial setup

---
+++ Social Lives/Social Lives/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Social Lives
+//
+//  Created by Kyle Watson on 7/15/14.
+//  Copyright (c) 2014 Kyle Watson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Social Lives/Social LivesTests/Social_LivesTests.m
@@ -0,0 +1,34 @@
+//
+//  Social_LivesTests.m
+//  Social LivesTests
+//
+//  Created by Kyle Watson on 7/15/14.
+//  Copyright (c) 2014 Kyle Watson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Social_LivesTests : XCTestCase
+
+@end
+
+@implementation Social_LivesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
